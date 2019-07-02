d73f7ab6d5da2f815bde08b459561501d098602c
Raphaël Pinson <raphael.pinson@camptocamp.com>
2019-07-02 12:13:51 +0200

    Lint

--- spec/functions/gopass_spec.rb
+++ spec/functions/gopass_spec.rb
@@ -12,51 +12,51 @@ describe 'gopass' do
   }
 
   context 'when secret does not exist' do
-    it 'should return nil' do
+    it 'returns nil' do
       Puppet::Util::Execution.expects(:execute).with('gopass list --flat').returns("foo\nbar\nbaz\n")
       is_expected.to run.with_params('test').and_return(nil)
     end
   end
 
   context 'when secret has password and yaml body' do
-    it 'should parse password, body and data' do
+    it 'parses password, body and data' do
       Puppet::Util::Execution.expects(:execute).with('gopass list --flat').returns("foo\nbar\nbaz\n")
       Puppet::Util::Execution.expects(:execute).with("gopass 'foo'").returns("mypasswd\n---\nkey: val\nkey2: ['val10', 'val11']\n")
 
-      is_expected.to run.with_params('foo').and_return({
+      is_expected.to run.with_params('foo').and_return(
         'password' => 'mypasswd',
         'body'     => "---\nkey: val\nkey2: ['val10', 'val11']",
         'data'     => {
           'key'  => 'val',
-          'key2' => [ 'val10', 'val11'],
+          'key2' => ['val10', 'val11'],
         },
-      })
+      )
     end
   end
 
   context 'when secret has password only' do
-    it 'should parse password, body and data' do
+    it 'parses password only' do
       Puppet::Util::Execution.expects(:execute).with('gopass list --flat').returns("foo\nbar\nbaz\n")
       Puppet::Util::Execution.expects(:execute).with("gopass 'foo'").returns("mypasswd\n")
 
-      is_expected.to run.with_params('foo').and_return({
+      is_expected.to run.with_params('foo').and_return(
         'password' => 'mypasswd',
         'body'     => nil,
         'data'     => nil,
-      })
+      )
     end
   end
 
   context 'when secret has password and blob body' do
-    it 'should parse password, body and data' do
+    it 'parses password and body only' do
       Puppet::Util::Execution.expects(:execute).with('gopass list --flat').returns("foo\nbar\nbaz\n")
       Puppet::Util::Execution.expects(:execute).with("gopass 'foo'").returns("mypasswd\nthis is a \nrandom: unparsable body\n")
 
-      is_expected.to run.with_params('foo').and_return({
+      is_expected.to run.with_params('foo').and_return(
         'password' => 'mypasswd',
         'body'     => "this is a \nrandom: unparsable body",
         'data'     => nil,
-      })
+      )
     end
   end
 end
